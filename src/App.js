31f85367a9a1867310ed1ffa4e193f4176b6fec4
silversoftwerks <51791643+silversoftwerks@users.noreply.github.com>
2022-02-17 17:20:25 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,11 +36,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      {//<img src={logo} className="App-logo" alt="logo" />}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+              Have you seen this guy?
+      {//Edit <code>src/App.js</code> and save to reload.}
           </p>
-          <LambdaDemo />
+  {  //   <LambdaDemo />}
         </header>
       </div>
     )
