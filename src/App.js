3a06877d59dbc8ef5c1d62d0d77f1c6c2131df19
silversoftwerks <51791643+silversoftwerks@users.noreply.github.com>
2022-02-17 18:14:50 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import useGaTracker from './useGaTracker'
 import logo from "./logo.svg"
 import "./App.css"
