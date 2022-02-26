20efa4f8c02e816ca01ced39314455bbdcbfc5a0
silversoftwerks <51791643+silversoftwerks@users.noreply.github.com>
2022-02-26 00:25:51 -0700

    k

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -23,3 +23,15 @@ export function handler(event, context, callback) {
     body: response,
   })
 }
+/*const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion("text-davinci-001", {
+  prompt: "Hello world",
+});
+console.log(completion.data.choices[0].text);
+*/
