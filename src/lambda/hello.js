c5de5f3f6f8ed62e3a73fe3b856f26f7b7b05550
silversoftwerks <51791643+silversoftwerks@users.noreply.github.com>
2022-02-26 00:17:23 -0700

    t

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,25 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion("text-davinci-001", {
+  prompt: "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
+  temperature: 0.5,
+  max_tokens: 60,
+  top_p: 1,
+  frequency_penalty: 0.5,
+  presence_penalty: 0,
+  stop: ["You:"],
+});
+
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
 export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
+    body: response,
   })
 }
